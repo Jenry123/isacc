c15cb57f469d8f759fb07d4f8b6a9fb1993dbd30
jenry eduardo <prueba@email.com>
2024-03-06 19:45:57 -0600

    actualizado

--- Almacen.java
+++ Almacen.java
@@ -1,7 +1,15 @@
 package isacc;
 
 public class  Almacen {
+
     public void main(String[] args) {
-        
+        System.out.println("hola bebe");
+        mostrar();
     }
+
+
+    public static void mostrar(){
+        System.out.println("que onda UwU");
+    }
+
 }
