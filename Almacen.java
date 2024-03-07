77cb290119fe7bc5800c4c5f66512efbfde1337b
jenry eduardo <prueba@email.com>
2024-03-06 21:53:26 -0600

    actualizacion

--- Almacen.java
+++ Almacen.java
@@ -2,14 +2,4 @@ package isacc;
 
 public class  Almacen {
 
-    public static void main(String[] args) {
-        System.out.println("hola bebe");
-        mostrar();
-    }
-
-
-    public static void mostrar(){
-        System.out.println("que onda UwU");
-    }
-
 }
--- MenuDeposito.java
+++ MenuDeposito.java
@@ -1,1 +1,8 @@
 package isacc;
+
+public class MenuDeposito {
+
+    public static void main(String[] args) {
+ 
+    }
+}
