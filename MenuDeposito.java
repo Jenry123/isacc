817e918dbcd44a44eb508c45848a4a793a65f075
jenry eduardo <prueba@email.com>
2024-03-06 22:20:03 -0600

    actualizacion

--- MenuDeposito.java
+++ MenuDeposito.java
@@ -1,8 +1,26 @@
 package isacc;
-
+import java.util.Scanner;
 public class MenuDeposito {
 
+
     public static void main(String[] args) {
- 
+
+        verificarUsuario();
     }
+
+    public static void verificarUsuario(){
+        Usuario listUsuario[]=new Usuario[5];
+        Scanner guardar=new Scanner(System.in);
+        String usuario;
+        System.out.println("ingrese su usuario");
+        usuario =guardar.nextLine();
+
+        for(int i=0; i<listUsuario.length; i++){
+                if(usuario==listUsuario[i].getUsuario()){
+                    
+                }
+        }
+
+    }
+    
 }
--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,21 @@
 package isacc;
+
+public class Usuario {
+
+    private String usuario;
+    private String contrasena;
+
+    public void setUsuario(String usuario,String contrasena){
+        this.usuario = usuario;
+        this.contrasena=contrasena;
+    }
+
+    public String  getUsuario(){
+         return usuario;
+    }
+
+    public String getContrasena(){
+        return  contrasena;
+    }
+    
+}
