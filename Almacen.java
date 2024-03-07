e37586aedfae32ca075d26d062ba6e082f5de116
jenry eduardo <prueba@email.com>
2024-03-06 19:57:11 -0600

    ok

--- Almacen.java
+++ Almacen.java
@@ -2,7 +2,7 @@ package isacc;
 
 public class  Almacen {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("hola bebe");
         mostrar();
     }
